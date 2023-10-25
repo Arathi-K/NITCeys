a7f018cc51c1753c68f1089b2c0b684c223784b2
Vaisakh <realsharqi@gmail.com>
2023-10-25 22:26:52 +0530

    faculty and admin validation

--- index.js
+++ index.js
@@ -6,32 +6,53 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'UI', 'login.html'));
+  res.sendFile(path.join(__dirname, 'UI', 'login.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server successfully running on port ${port}`);
+  console.log(`Server successfully running on port ${port}`);
 
-    exec(`start http://localhost:${port}`, (error) => {
-        if (error) {
-            console.error('Error opening browser:', error);
-        }
-    });
+  exec(`start http://localhost:${port}`, (error) => {
+    if (error) {
+      console.error('Error opening browser:', error);
+    }
+  });
 });
 
-app.post('/',(req,res)=>{
-    var username = req.body.username.trim();
-    var password = req.body.password.trim();
-    var sqlQuery = "select * from user where User_id = ? and Password = ?";
-    con.query(sqlQuery,[username, password],function(err,result){
-      if(err) throw err;
-      if(result.length==0){
+app.post('/', (req, res) => {
+  var username = req.body.username.trim();
+  var password = req.body.password.trim();
+  if (username[0] === 'A') {
+    var sqlQuery = "select * from admin where Admin_id = ? and Password = ?";
+    con.query(sqlQuery, [username, password], function (err, result) {
+      if (err) throw err;
+      if (result.length == 0) {
         res.send("User doesnt exist");
-      }else{
+      } else {
         res.send("Data received");
       }
     });
+  } else {
+    var sqlQuery = "select * from user where User_id = ? and Password = ?";
+    con.query(sqlQuery, [username, password], function (err, result) {
+      if (err) throw err;
+      if (result.length == 0) {
+        res.send("User doesnt exist");
+      } else {
+        res.send("Data received");
+      }
+    });
+  }
+  // var sqlQuery = "select * from user where User_id = ? and Password = ?";
+  // con.query(sqlQuery,[username, password],function(err,result){
+  //   if(err) throw err;
+  //   if(result.length==0){
+  //     res.send("User doesnt exist");
+  //   }else{
+  //     res.send("Data received");
+  //   }
+  // });
 })

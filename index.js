c22a797780c63a56ee1870300de334c23f90bfe6
Vaisakh <realsharqi@gmail.com>
2023-10-26 03:11:50 +0530

    redirecting to dashboards

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const path = require('path');
 const { exec } = require('child_process');
 const app = express();
 const port = 8080;
+const http = require('http');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/UI', express.static(path.join(__dirname, 'UI')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'UI', 'login.html'));
+});
+
+app.get('/UI/studentdashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'UI', 'studentdashboard.html'));
+});
+
+app.get('/UI/adminDashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'UI', 'adminDashboard.html'));
 });
 
 app.listen(port, () => {
@@ -30,9 +40,11 @@ app.post('/', (req, res) => {
     con.query(sqlQuery, [username, password], function (err, result) {
       if (err) throw err;
       if (result.length == 0) {
-        res.send("User doesnt exist");
+        // res.send("User doesnt exist");
+        const alertScript = `<script>alert('User does not exist.');window.location.href = '/UI/login.html';</script>`;
+        res.send(alertScript);
       } else {
-        res.send("Data received");
+        res.redirect('/UI/adminDashboard.html');
       }
     });
   } else {
@@ -40,19 +52,13 @@ app.post('/', (req, res) => {
     con.query(sqlQuery, [username, password], function (err, result) {
       if (err) throw err;
       if (result.length == 0) {
-        res.send("User doesnt exist");
+        // res.send("User doesnt exist");
+        const alertScript = `<script>alert('User does not exist.');window.location.href = '/UI/login.html';</script>`;
+        res.send(alertScript);
       } else {
-        res.send("Data received");
+        // res.send("Data received");
+        res.redirect('/UI/studentdashboard.html');
       }
     });
   }
-  // var sqlQuery = "select * from user where User_id = ? and Password = ?";
-  // con.query(sqlQuery,[username, password],function(err,result){
-  //   if(err) throw err;
-  //   if(result.length==0){
-  //     res.send("User doesnt exist");
-  //   }else{
-  //     res.send("Data received");
-  //   }
-  // });
 })
